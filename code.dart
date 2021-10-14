80f7aed2f6172f86f70cc6de668ae88a586d8129
coderman0 <81552900+coderman0@users.noreply.github.com>
2021-10-14 20:07:24 +0300

    Add files via upload

---
+++ code.dart
@@ -0,0 +1,31 @@
+void main(){
+
+
+  
+  
+  
+  String ad2 = 'Ahmet2';
+  int sayi = 15;
+  double kilogram = 21.1;
+  num boy = 185.3;
+  // num = int + double
+  
+  const pi = 3.14; //sabit değişken bunu değiştiremezsin
+  
+  List <String> meyveler = ['portakal', 'Elma', 'Çilek', 'Armut'];
+                               // 0           1       2        3
+  
+  
+ print('Benim Kilom: $kilogram'); 
+ print('Lisstenin 2.meyvesi: ${meyveler[1]}');
+ print(meyveler[1]);
+ print(ad2);
+ print(boy);  
+  
+  
+  
+  
+  
+  
+  
+}
